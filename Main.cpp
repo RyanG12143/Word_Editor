32335031b2561d6fe876c21d56c5e88d9cb2e88c
RyanG12143 <145314969+RyanG12143@users.noreply.github.com>
2024-10-31 20:57:33 -0500

    Working window

--- Main.cpp
+++ Main.cpp
@@ -5,85 +5,79 @@
 #include <iostream>
 #include <Windows.h>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int main(){
-    std::cout<< "Hello World" << std::endl;
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    // Register the window class.
+    const wchar_t CLASS_NAME[]  = L"Sample Window Class";
+    
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"Learn to Program Windows",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+        );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
 
-// LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
 
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
 
-// int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
-// {
-//     // Register the window class.
-//     const wchar_t CLASS_NAME[]  = L"Sample Window Class";
-    
-//     WNDCLASS wc = { };
+            // All painting occurs here, between BeginPaint and EndPaint.
 
-//     wc.lpfnWndProc   = WindowProc;
-//     wc.hInstance     = hInstance;
-//     wc.lpszClassName = CLASS_NAME;
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
 
-//     RegisterClass(&wc);
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
 
-//     // Create the window.
-
-//     HWND hwnd = CreateWindowEx(
-//         0,                              // Optional window styles.
-//         CLASS_NAME,                     // Window class
-//         L"Learn to Program Windows",    // Window text
-//         WS_OVERLAPPEDWINDOW,            // Window style
-
-//         // Size and position
-//         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-//         NULL,       // Parent window    
-//         NULL,       // Menu
-//         hInstance,  // Instance handle
-//         NULL        // Additional application data
-//         );
-
-//     if (hwnd == NULL)
-//     {
-//         return 0;
-//     }
-
-//     ShowWindow(hwnd, nCmdShow);
-
-//     // Run the message loop.
-
-//     // MSG msg = { };
-//     // while (GetMessage(&msg, NULL, 0, 0) > 0)
-//     // {
-//     //     TranslateMessage(&msg);
-//     //     DispatchMessage(&msg);
-//     // }
-
-//     return 0;
-// }
-
-// LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-// {
-//     switch (uMsg)
-//     {
-//     case WM_DESTROY:
-//         PostQuitMessage(0);
-//         return 0;
-
-//     case WM_PAINT:
-//         {
-//             PAINTSTRUCT ps;
-//             HDC hdc = BeginPaint(hwnd, &ps);
-
-//             // All painting occurs here, between BeginPaint and EndPaint.
-
-//             FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
-
-//             EndPaint(hwnd, &ps);
-//         }
-//         return 0;
-
-//     }
-//     return DefWindowProc(hwnd, uMsg, wParam, lParam);
-// }
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
